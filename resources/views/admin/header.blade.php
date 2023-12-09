153079764dee59b08daba238d9ee2a27631fb7e2
Samandar Sariboyev <samandarsariboyev69@gmail.com>
2023-12-09 23:36:48 +0500

    bug fixed

--- resources/views/admin/header.blade.php
+++ resources/views/admin/header.blade.php
@@ -8,7 +8,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="Responsive Admin &amp; Dashboard Template based on Bootstrap 5">
     <meta name="author" content="Quizgram">
-    <meta name="keywords" content="quiz, test, prezident maktabi, savollar">
+    <meta name="keywords" content="quizgram
+    , test, prezident maktabi, savollar">
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="shortcut icon" href="{{ asset('img/icons/icon-48x48.png') }}" />
@@ -37,8 +38,6 @@
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sliders align-middle"><line x1="4" y1="21" x2="4" y2="14"></line><line x1="4" y1="10" x2="4" y2="3"></line><line x1="12" y1="21" x2="12" y2="12"></line><line x1="12" y1="8" x2="12" y2="3"></line><line x1="20" y1="21" x2="20" y2="16"></line><line x1="20" y1="12" x2="20" y2="3"></line><line x1="1" y1="14" x2="7" y2="14"></line><line x1="9" y1="8" x2="15" y2="8"></line><line x1="17" y1="16" x2="23" y2="16"></line></svg> <span class="align-middle">Bosh menu</span>
                     </a>
                 </li>
-
-
 
 
                 <li class="sidebar-header">
